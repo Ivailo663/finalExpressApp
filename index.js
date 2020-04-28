840cb91ed8eee80dc2b2306830e724f7ab86d90f
Georgiev <igeorgiev21@dxc.com>
2020-04-28 16:08:05 +0300

    commit all the crap !!!

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const PORT = process.env.PORT || 3002;
+
+// app.get("/", (req, res) => {
+//   res.status(200).send("Hello from the server");
+// });
+// app.use("/static", express.static("client/public"));
+app.use(express.static("./client/build/"));
+
+// app.get("/", function (req, res) {
+//   res.sendFile("./client/build/");
+// });
+
+app.listen(PORT, () => {
+  console.log("listening...");
+});
