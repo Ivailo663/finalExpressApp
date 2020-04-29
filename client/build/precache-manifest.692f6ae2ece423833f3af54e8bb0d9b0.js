a2416e9d0f9f67396911039f58531b3536e7eb26
Georgiev <igeorgiev21@dxc.com>
2020-04-29 16:04:35 +0300

    push build

---
+++ client/build/precache-manifest.692f6ae2ece423833f3af54e8bb0d9b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eedcbb199bd27cd608f18e0eaf2d987",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f2ed4b2b5f95b719378",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fd2d49542ab5cc52d970",
+    "url": "/static/js/2.7e2ee095.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7e2ee095.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f2ed4b2b5f95b719378",
+    "url": "/static/js/main.51e78980.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5002;
 app.use(express.static("./client/build/"));
 
 app.get("*", (req, res) => {
-  res.sendFile("client/build/index.html", { root: __dirname });
+  // res.sendFile("client/build/index.html", { root: __dirname });
 });
 
 app.listen(PORT, () => {
