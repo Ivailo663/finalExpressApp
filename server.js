0b1113d02f548af1518c23d19f59830b85d283ba
Georgiev <igeorgiev21@dxc.com>
2020-04-29 15:38:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const PORT = process.env.PORT || 5002;
 
 app.use(express.static("./client/build/"));
 
+app.get("*", (req, res) => {
+  res.sendFile("index.html", { root: __dirname + "client/build" });
+});
+
 app.listen(PORT, () => {
   console.log("listening....");
 });
