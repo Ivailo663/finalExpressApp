132829f78bd489561c5a5748ea7bdd405b9782bd
Georgiev <igeorgiev21@dxc.com>
2020-04-29 11:08:15 +0300

    attempt

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const app = express();
 
 const PORT = process.env.PORT || 5002;
 
-app.get("/", (req, res) => {
-  res.send("hello from server");
-});
+// app.get("/", (req, res) => {
+//   res.send("hello from server");
+// });
+
+app.use(express.static("./client/build/"));
 
 app.listen(PORT, () => {
   console.log("listening....");
