b923b67f4b2f33fd77e0e06a1606431f55531593
Georgiev <igeorgiev21@dxc.com>
2020-04-30 10:49:00 +0300

    ignored

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
+
+const PORT = process.env.PORT || 5002;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("./client/build/"));
+  app.get("*", (req, res) => {
+    res.sendFile("index.html", {
+      root: path.join(__dirname, "./client/build"),
+    });
+  });
+
+app.listen(PORT, () => {
+  console.log("Listening...", process.env.NODE_ENV);
+});
