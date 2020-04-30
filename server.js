8f31f59cde783396a44a0427e312aa010703d0d9
Georgiev <igeorgiev21@dxc.com>
2020-04-30 13:18:58 +0300

    ads

--- server.js
+++ server.js
@@ -7,13 +7,15 @@ dotenv.config({ path: "./config.env" });
 
 const PORT = process.env.PORT || 5002;
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "development") {
   app.use(express.static("./client/build/"));
   app.get("*", (req, res) => {
     res.sendFile("index.html", {
       root: path.join(__dirname, "./client/build"),
     });
   });
+  console.log("HEYYYYYYYYYYYYYYYY");
+}
 
 app.listen(PORT, () => {
   console.log("Listening...", process.env.NODE_ENV);
